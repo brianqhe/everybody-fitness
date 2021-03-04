b20c5a5cd87d05ca07f05e258b22f61e0a046862
brianskiies <brian.he3@gmail.com>
2021-03-04 12:16:15 +1100

    added bootstrap navbar

--- db/migrate/20210304010859_devise_create_users.rb
+++ db/migrate/20210304010859_devise_create_users.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-class DeviseCreateTrainers < ActiveRecord::Migration[6.1]
+class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
-    create_table :trainers do |t|
+    create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -36,9 +36,9 @@ class DeviseCreateTrainers < ActiveRecord::Migration[6.1]
       t.timestamps null: false
     end
 
-    add_index :trainers, :email,                unique: true
-    add_index :trainers, :reset_password_token, unique: true
-    # add_index :trainers, :confirmation_token,   unique: true
-    # add_index :trainers, :unlock_token,         unique: true
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :unlock_token,         unique: true
   end
 end
