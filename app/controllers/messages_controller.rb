4e6dda23d5866111c909c885376c30e4cb7108fb
brianskiies <brian.he3@gmail.com>
2021-03-18 14:06:28 +1100

    added code comments for stlyesheets and controllers

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,14 @@
 class MessagesController < ApplicationController
+  # authenticate user before being able to access any routes for messages
   before_action :authenticate_user!
 
+  # Setting an instance variable to find the relevant conversation for a user
   before_action do
     @conversation = Conversation.find(params[:conversation_id])
   end
 
   def index
+    # Setting instance variable for messages for the current user that is logged in
     @messages = @conversation.messages
 
     @messages.where("user_id != ? AND read = ?", current_user.id, false).update_all(read: true)
@@ -14,7 +17,9 @@ class MessagesController < ApplicationController
   end
 
   def create
+    # Creating a new message based on the conversation that has been set
     @message = @conversation.messages.new(message_params)
+    # Allocating the foreign key required for the message model that links to the user model
     @message.user = current_user
 
     if @message.save
